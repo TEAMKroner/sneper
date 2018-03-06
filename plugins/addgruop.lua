0afef57f3110f788f1fc74e612dbd67ac571852c
TEEMKroner <37085703+TEEMKroner@users.noreply.github.com>
2018-03-06 20:12:22 +0300

    Update addgruop.lua

--- plugins/addgruop.lua
+++ plugins/addgruop.lua
@@ -2,7 +2,7 @@ local function run(msg,STORM)
   local key = 'chat:gp'..msg.to.id
   if STORM[1] == 'اضف رد' and is_mod(msg) then
     redis:hset(key,STORM[2],STORM[3])
-    return "👁‍🗨￤» الــكلمــه {`"..STORM[2].."`}\n👁‍🗨￤» الــرد {`"..STORM[3].."`}\n👁‍🗨￤» تــم اضافتــها الــى قائمــه الردود"
+    return "👁‍🗨￤» دز الكلمة {`"..STORM[2].."`}\n👁‍🗨￤» دز الرد {`"..STORM[3].."`}\n👁‍🗨￤» تــم اضافتــها الــى قائمــه الردود"
   end
   
   if STORM[1] == 'حذف رد' and is_mod(msg) then
